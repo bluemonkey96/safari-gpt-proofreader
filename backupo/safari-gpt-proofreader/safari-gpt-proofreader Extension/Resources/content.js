4ea73ad590f58eb75c9b0f82acfe313eede2636e
bluemonkey96 <andersaminoff@gmail.com>
2025-10-17 23:19:35 +0200

    Stop tracking backup folder (contained secret)

--- backupo/safari-gpt-proofreader/safari-gpt-proofreader Extension/Resources/content.js
+++
@@ -1,6 +0,0 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Message received in content script:", request.message);  // Log the message received from background
-    if (request.message) {
-        alert(request.message);  // Show the message passed from the background script
-    }
-});
